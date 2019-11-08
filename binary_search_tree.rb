4592e686819c6c8040cc2d4583f46eb90d31de52
scottrobel <robelrain@gmail.com>
2019-11-08 01:22:18 -0500

    added insert method to insert a value into the tree with the helper methods 'insert_left_node' and 'insert_right_node'

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -9,26 +9,62 @@ class Node
     self.data = data
   end
 end
+# Tree class creates a balanced node tree
+# from an array when initialized
+# Does not handle duplicate values
+class Tree
+  attr_reader :root_node
+  def initialize(array)
+    @root_node = build_tree(array)
+  end
 
-def build_tree(array)
-  array.uniq!
-  array.sort!
-  return Node.new(array[0]) if array.size == 1
-  return nil if array.empty?
+  def build_tree(array)
+    array.uniq!
+    array.sort!
+    return Node.new(array[0]) if array.size == 1
+    return nil if array.empty?
 
-  smaller_values, center_value, greater_values = get_node_branch_arrays(array)
-  node = Node.new(center_value)
-  node.right_link = build_tree(greater_values)
-  node.left_link = build_tree(smaller_values)
-  node
+    smaller_values, center_value, greater_values = get_node_branch_arrays(array)
+    node = Node.new(center_value)
+    node.right_link = build_tree(greater_values)
+    node.left_link = build_tree(smaller_values)
+    node
+  end
+
+  def insert(data, node = root_node)
+    if data > node.data
+      insert_right_node(data, node)
+    elsif data < node.data
+      insert_left_node(data, node)
+    end
+  end
+
+  private
+
+  def insert_right_node(data, node)
+    if node.right_link.nil?
+      node.right_link = Node.new(data)
+    else
+      insert(data, node.right_link)
+    end
+  end
+
+  def insert_left_node(data, node)
+    if node.left_link.nil?
+      node.left_link = Node.new(data)
+    else
+      insert(data, node.left_link)
+    end
+  end
+
+  def get_node_branch_arrays(array)
+    center_value = array[array.size / 2]
+    smaller_values = array[0..array.size / 2] - [center_value]
+    greater_values = array[array.size / 2..array.length - 1] - [center_value]
+    [smaller_values, center_value, greater_values]
+  end
 end
-
-def get_node_branch_arrays(array)
-  center_value = array[array.size / 2]
-  smaller_values = array[0..array.size / 2] - [center_value]
-  greater_values = array[array.size / 2..array.length - 1] - [center_value]
-  [smaller_values, center_value, greater_values]
-end
-
-my_tree = build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+my_tree = Tree.new([1, 2, 3])
+my_tree.insert(4)
+root = my_tree.root_node
 pry.binding
